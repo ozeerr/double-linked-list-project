31f67fb5a924b15ca8b0b6dfcb20acfa1e051da5
Ahmet Özer <efe.aeo@gmail.com>
2024-02-28 00:24:25 +0300

    double-linked-list

---
+++ odev3/odev3/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace odev3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUrunIsmi = new System.Windows.Forms.TextBox();
+            this.txtUrunKodu = new System.Windows.Forms.TextBox();
+            this.txtUrunFiyati = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnList = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUrunIsmi
+            // 
+            this.txtUrunIsmi.Location = new System.Drawing.Point(134, 56);
+            this.txtUrunIsmi.Name = "txtUrunIsmi";
+            this.txtUrunIsmi.Size = new System.Drawing.Size(158, 20);
+            this.txtUrunIsmi.TabIndex = 0;
+            // 
+            // txtUrunKodu
+            // 
+            this.txtUrunKodu.Location = new System.Drawing.Point(134, 114);
+            this.txtUrunKodu.Name = "txtUrunKodu";
+            this.txtUrunKodu.Size = new System.Drawing.Size(158, 20);
+            this.txtUrunKodu.TabIndex = 1;
+            // 
+            // txtUrunFiyati
+            // 
+            this.txtUrunFiyati.Location = new System.Drawing.Point(134, 172);
+            this.txtUrunFiyati.Name = "txtUrunFiyati";
+            this.txtUrunFiyati.Size = new System.Drawing.Size(158, 20);
+            this.txtUrunFiyati.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(330, 37);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(111, 57);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "EKLE";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(493, 150);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(100, 57);
+            this.btnList.TabIndex = 6;
+            this.btnList.Text = "LİSTELE";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.Location = new System.Drawing.Point(48, 248);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(545, 179);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(330, 150);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(105, 56);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "GÜNCELLE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(493, 37);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 57);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "SİL";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "ÜRÜN İSMİ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "ÜRÜN KODU";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "ÜRÜN FİYATI";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(238, 455);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "AHMET EFE ÖZER  223908025";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 485);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(615, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Hocam ek puan olan DataGridView\'e tıklandığında textboxlara gelme olayını yaptım." +
+    " Buna ek olarak TabIndex ayarlarını da yaptım.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(649, 540);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtUrunFiyati);
+            this.Controls.Add(this.txtUrunKodu);
+            this.Controls.Add(this.txtUrunIsmi);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrunIsmi;
+        private System.Windows.Forms.TextBox txtUrunKodu;
+        private System.Windows.Forms.TextBox txtUrunFiyati;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ odev3/odev3/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odev3
+{
+    public partial class Form1 : Form
+    {
+
+        private ProductList productList = new ProductList();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Product newProduct = new Product
+            {
+                UrunIsmi = txtUrunIsmi.Text,
+                UrunKodu = txtUrunKodu.Text,
+                UrunFiyati = txtUrunFiyati.Text
+            };
+            productList.AddProduct(newProduct);
+            ClearTextBoxes();
+          
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            ShowProductsInDataGridView();
+
+        }
+        private void ShowProductsInDataGridView()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = productList.GetProductList();
+        }
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+                txtUrunIsmi.Text = selectedRow.Cells["UrunIsmi"].Value.ToString();
+                txtUrunKodu.Text = selectedRow.Cells["UrunKodu"].Value.ToString();
+                txtUrunFiyati.Text = selectedRow.Cells["UrunFiyati"].Value.ToString();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string UrunKoduToUpdate = txtUrunKodu.Text;
+            Product updatedProduct = new Product
+            {
+                UrunIsmi = txtUrunIsmi.Text,
+                UrunKodu = txtUrunKodu.Text,
+                UrunFiyati = txtUrunFiyati.Text
+            };
+            productList.UpdateProductByCode(UrunKoduToUpdate, updatedProduct);
+            ClearTextBoxes();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+            string UrunKoduToDelete = txtUrunKodu.Text;
+            productList.DeleteProductByCode(UrunKoduToDelete);
+           
+            ClearTextBoxes();
+
+        }
+        private void ClearTextBoxes()
+        {
+            txtUrunIsmi.Text = "";
+            txtUrunKodu.Text = "";
+            txtUrunFiyati.Text = "";
+        }
+
+        private void txtUrunIsmi_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0) 
+            {
+                DataGridViewRow selectedRow = dataGridView1.Rows[e.RowIndex];
+                txtUrunIsmi.Text = selectedRow.Cells["UrunIsmi"].Value.ToString();
+                txtUrunKodu.Text = selectedRow.Cells["UrunKodu"].Value.ToString();
+                txtUrunFiyati.Text = selectedRow.Cells["UrunFiyati"].Value.ToString();
+            }
+        }
+
+       
+    }
+    public class Product
+    {
+        public string UrunIsmi { get; set; }
+        public string UrunKodu { get; set; }
+        public string UrunFiyati { get; set; }
+    }
+
+    public class Node
+    {
+        public Product Data;
+        public Node Next;
+        public Node Prev;
+    }
+
+    public class ProductList
+    {
+        private Node head;
+
+        public void AddProduct(Product product)
+        {
+            Node newNode = new Node { Data = product };
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+                newNode.Prev = current;
+            }
+        }
+
+        public void DeleteProductByCode(string UrunKodu)
+        {
+            Node nodeToDelete = FindNodeByCode(UrunKodu);
+            if (nodeToDelete != null)
+            {
+                if (nodeToDelete.Prev != null)
+                {
+                    nodeToDelete.Prev.Next = nodeToDelete.Next;
+                }
+                if (nodeToDelete.Next != null)
+                {
+                    nodeToDelete.Next.Prev = nodeToDelete.Prev;
+                }
+                if (nodeToDelete == head)
+                {
+                    head = nodeToDelete.Next;
+                }
+            }
+        }
+
+        private Node FindNodeByCode(string code)
+        {
+            Node current = head;
+            while (current != null)
+            {
+                if (current.Data.UrunKodu == code)
+                {
+                    return current;
+                }
+                current = current.Next;
+            }
+            return null;
+        }
+
+        public void UpdateProductByCode(string UrunKodu, Product updatedProduct)
+        {
+            Node nodeToUpdate = FindNodeByCode(UrunKodu);
+            if (nodeToUpdate != null)
+            {
+                nodeToUpdate.Data = updatedProduct;
+            }
+        }
+        private Node FindNode(Product product)
+        {
+            Node current = head;
+            while (current != null)
+            {
+                if (current.Data == product)
+                {
+                    return current;
+                }
+                current = current.Next;
+            }
+            return null;
+        }
+        public List<Product> GetProductList()
+        {
+            List<Product> productList = new List<Product>();
+            Node current = head;
+            while (current != null)
+            {
+                productList.Add(current.Data);
+                current = current.Next;
+            }
+            return productList;
+        }
+    }
+  
+}
